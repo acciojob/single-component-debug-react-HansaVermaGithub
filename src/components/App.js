ba52e67d92336a62e5fd53c9693cb99b8cc9ac24
HansaVermaGithub <hansaverma88@gmail.com>
2026-01-09 13:47:37 +0530

    Update App component structure and export
    Fix rendering function and export statement.

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,9 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-
+import React from 'react'
+function App(){
+  render(
+    <div id="main">
+    <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+  )
+}
+export defaut App.js
