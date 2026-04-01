d709f66efef5cd90918b0ea21f0aca83e3fd8f3f
unknown <hansaverma88@gmail.com>
2026-04-01 22:37:28 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
-function App(){
-  render(
+import React from 'react';
+const  App = () => {
+  return(
     <div id="main">
-    <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+     <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
     </div>
-  )
+  );
+
 }
-export default App.js
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-const container=document.getElementById('root');
-const root= reactDOM.createRoot(container);
-root.render(<App/>);
+//const container=document.getElementById('root');
+//const root= ReactDOM.createRoot(container);
+//root.render(<App/>);
 
+ReactDOM.render(<App/>, document .getElementById("root"));
